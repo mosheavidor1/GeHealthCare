980be527dd60790017894397dc768a0bc23ec444
mosheavidor1 <47886429+mosheavidor1@users.noreply.github.com>
2023-01-20 01:26:03 +0200

    d

--- src/main/java/Test/TestUI.java
+++ src/main/java/Test/TestUI.java
@@ -10,6 +10,9 @@ import org.testng.annotations.Test;
 
 import java.util.List;
 
+import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.assertTrue;
+
 public class TestUI {
     private WebDriver driver;
 
@@ -39,31 +42,41 @@ public class TestUI {
         Thread.sleep(1000);
         driver.findElement(By.xpath("//span[normalize-space()='Time']")).click();
         driver.manage().window().fullscreen();
+
         Thread.sleep(1000);
 
+        driver.findElement(By.xpath("(//button[@type='button'][normalize-space()='View'])[2]")).click();
 
-        WebElement date = driver.findElement(By.xpath("//div[contains(text(),'2022-08-15 - 2022-08-21')]"));
-        List<WebElement> views = date.findElements(By.tagName("View"));
-        for (WebElement view : views) {
-            if (view.getText().equals("View")) {
-                view.click();
-                break;
+        driver.manage().window().fullscreen();
 
-            }
-        }
+        Thread.sleep(1000);
 
-//
-//        WebElement viewInDate = driver.findElement(By.xpath("//div[contains(text(),'2022-08-15 - 2022-08-21')]"));
-//        WebElement view=driver.findElement(By.xpath("(//button[@type='button'][normalize-space()='View'])[2]"));
-//
-//        if(viewInDate.isDisplayed()) {
-//            view.click();
-//        }
+        driver.findElement(By.cssSelector(".oxd-icon.bi-chat-dots-fill")).click();
+        Thread.sleep(1000);
 
+        WebElement comment = driver.findElement(By.xpath("//textarea[@placeholder='Comment here']"));
+        String commentText = comment.getAttribute("value");
+        System.out.println(commentText);
+        assertEquals("Leadership Development", commentText);
+
+        //For the negative test :
+       // assertEquals("This is a negative test", commentText);
+        //System.out.println(driver.findElement(By.xpath("//textarea[@placeholder='Comment here']")).getAttribute("value"));
 
 
-            }
-        }
+
+        //Use this for the date compare to view button because it has 'and' option on xpath.
+        // System.out.println(driver.findElement(By.xpath("//input[@class='inputAlign optionalHintText' and @id='Amount']")).getAttribute("value"));
+
+
+//Leadership Development
+
+    }
+
+    }
+
+
+
 
 
 
